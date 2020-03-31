d1825f1af9c54a726db32fb511b7b6b165f287b7
Sumit <sumitppawar@gmail.com>
2020-03-31 16:13:41 +0800

    Remove this

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ else
 	SONAR_PASSWORD="${INPUT_PASSWORD}"
 fi
 
-sonar-scanner \
-  -Dsonar.projectKey=key \
-  -Dsonar.host.url=http://host \
-  -Dsonar.login=token
+  sonar-scanner \
+  -Dsonar.projectKey=Gladiator \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://3.209.122.243:9000 \
+  -Dsonar.login=e0a8dc8cd673b666c1093c4d84eb32e8390826ea
+  -Dsonar.analysis.mode=preview \
+  -Dsonar.issuesReport.html.enable=true \
 
