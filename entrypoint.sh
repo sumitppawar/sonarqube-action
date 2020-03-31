716cd53961ae66811126e64e89c14cb9077e6ab7
Sumit <sumitppawar@gmail.com>
2020-03-31 15:56:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ else
 fi
 
 sonar-scanner \
-  -Dsonar.projectKey=AMLS_API \
-  -Dsonar.host.url=http://3.209.122.243:9000 \
-  -Dsonar.login=ba7e11fbc02595f999e47f120444fcd26fdcc1d8
+  -Dsonar.projectKey=key \
+  -Dsonar.host.url=http://host \
+  -Dsonar.login=token
 
