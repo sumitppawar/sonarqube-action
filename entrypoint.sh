63c38ed4c32021bafa2867e0d486c4054e09bfde
Sumit <sumitppawar@gmail.com>
2020-03-31 16:16:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,5 @@ fi
   -Dsonar.host.url=http://3.209.122.243:9000 \
   -Dsonar.login=e0a8dc8cd673b666c1093c4d84eb32e8390826ea \
   -Dsonar.analysis.mode=preview \
-  -Dsonar.issuesReport.html.enable=true \
+  -Dsonar.issuesReport.html.enable=true 
 
